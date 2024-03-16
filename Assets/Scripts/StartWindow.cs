7213427c39ae2d80dee1de9657c0673f51b4d5fb
AtiM07 <atim203011@gmail.com>
2024-03-16 19:14:22 +0300

    Заданы параметры окон

--- Assets/Scripts/StartWindow.cs
+++ Assets/Scripts/StartWindow.cs
@@ -21,31 +21,32 @@ public class StartWindow : MonoBehaviour
         var model = count > 5
             ? new WindowModel()
             {
-                Header = "",
-                Description = "",
-                IconName = "",
+                Header = "Энергетический набор",
+                Description = "Этот набор для IT-пециалиста спасет любого понурого работника сферы информационных систем и технологий. Всего пара кружек кофе и ваш работник будет в ресурсе и в потоке!",
+                IconName = "зарядка",
                 Objects = new List<Object>
                 {
                     new Object { Name = "кофе", Count = 30 },
                     new Object { Name = "кофе", Count = 40 },
                     new Object { Name = "кофе", Count = 50 }
                 },
-                Price = 0,
-                Sale = 0
+                Price = 24,
+                Sale = 14
             }
             : new WindowModel()
             {
-                Header = "1111",
-                Description = "",
-                IconName = "",
+                Header = "Стартовый набор айтишника",
+                Description = "Необходимый набор предметов для человека со статусом в ВК \"Войти в айти\". Темные круги и недосып в подарок!",
+                IconName = "набор",
                 Objects = new List<Object>
                 {
-                    new Object { Name = "кофе", Count = 60 },
-                    new Object { Name = "кофе", Count = 70 },
-                    new Object { Name = "кофе", Count = 80 }
+                    new Object { Name = "кофе", Count = 100 },
+                    new Object { Name = "очки", Count = 60 },
+                    new Object { Name = "клавиатура", Count = 20 },
+                    new Object { Name = "деньги", Count = 1 }
                 },
-                Price = 0,
-                Sale = 0
+                Price = 65,
+                Sale = 26
             };
 
         GameManager.Instance.SetModelParameter(model);
--- Assets/Scripts/WindowView.cs
+++ Assets/Scripts/WindowView.cs
@@ -1,13 +1,18 @@
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class WindowView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI Header;
     [SerializeField] private TextMeshProUGUI Description;
     [SerializeField] private TextMeshProUGUI Price;
+    [SerializeField] private TextMeshProUGUI RealPrice;
     [SerializeField] private TextMeshProUGUI Sale;
+    [SerializeField] public Image MainIicon;
+    [SerializeField] private Sprite[] _itemsImages;
 
     public UnityEvent Init;
 
@@ -16,8 +21,20 @@ public class WindowView : MonoBehaviour
         var model = GameManager.Instance.Model;
         Header.text = model.Header;
         Description.text = model.Description;
-        Price.text = model.Price.ToString();
-        Sale.text = model.Sale.ToString();
+        SetPrice(RealPrice, GetRealPrice(model.Price, model.Sale));
+        SetPrice(Price, model.Price);
+        SetSale(model.Sale);
+        SetMainIcon(model.IconName);
         Init.Invoke();
+    }
+
+    private void SetSale(decimal sale) => Sale.text = sale + "%";
+    private void SetPrice(TextMeshProUGUI text, decimal price) => text.text = "$" + price;
+    
+    private decimal GetRealPrice(decimal price, decimal sale) => price*(1-sale/100);
+
+    private void SetMainIcon(string nameElement)
+    {
+        MainIicon.sprite = _itemsImages.FirstOrDefault(x => x.name == nameElement);
     }
 }
